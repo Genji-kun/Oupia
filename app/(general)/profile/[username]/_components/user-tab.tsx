cdb0cda73d0e6c70ae90dfed19dda118a73d1f20
Genji-kun <2051052097phat@ou.edu.vn>
2024-06-01 18:24:48 +0700

    [Update] Testing Cookies in Production

--- app/(general)/profile/[username]/_components/user-tab.tsx
+++ app/(general)/profile/[username]/_components/user-tab.tsx
@@ -1,71 +1,71 @@
-'use client';
+// 'use client';
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-import { Framer } from '@/lib/framer';
-import { LayoutGrid, UserRound, UsersRound } from 'lucide-react';
-import { useParams } from 'next/navigation';
-import { useProfileContext } from '@/contexts/profile-context';
-import { BsHouse } from 'react-icons/bs';
-import { useTabs } from '@/hooks';
+// import { Framer } from '@/lib/framer';
+// import { LayoutGrid, UserRound, UsersRound } from 'lucide-react';
+// import { useParams } from 'next/navigation';
+// import { useProfileContext } from '@/contexts/profile-context';
+// import { BsHouse } from 'react-icons/bs';
+// import { useTabs } from '@/hooks';
 
-const UserTab = () => {
-    const params = useParams();
-    const { username } = params;
-    const { userInfoData } = useProfileContext();
+// const UserTab = () => {
+//     const params = useParams();
+//     const { username } = params;
+//     const { userInfoData } = useProfileContext();
 
-    const defaultTabs = [
-        {
-            label:
-                <div className="flex gap-x-2 items-center">
-                    <LayoutGrid size="16" />
-                    <span>Tổng quan</span>
-                </div>,
-            id: 'gerenal',
-            link: `/profile/${username}`
-        },
-        {
-            label:
-                <div className="flex gap-x-2 items-center">
-                    <UserRound size="16" />
-                    <span>Thông tin</span>
-                </div>,
-            id: 'info',
-            link: `/profile/${username}/information`
-        },
-        {
-            label:
-                <div className="flex gap-x-2 items-center">
-                    <UsersRound size="16" />
-                    <span>Người theo dõi</span>
-                </div>,
-            id: 'follows',
-            link: `/profile/${username}/follows`
-        }
-    ];
+//     const defaultTabs = [
+//         {
+//             label:
+//                 <div className="flex gap-x-2 items-center">
+//                     <LayoutGrid size="16" />
+//                     <span>Tổng quan</span>
+//                 </div>,
+//             id: 'gerenal',
+//             link: `/profile/${username}`
+//         },
+//         {
+//             label:
+//                 <div className="flex gap-x-2 items-center">
+//                     <UserRound size="16" />
+//                     <span>Thông tin</span>
+//                 </div>,
+//             id: 'info',
+//             link: `/profile/${username}/information`
+//         },
+//         {
+//             label:
+//                 <div className="flex gap-x-2 items-center">
+//                     <UsersRound size="16" />
+//                     <span>Người theo dõi</span>
+//                 </div>,
+//             id: 'follows',
+//             link: `/profile/${username}/follows`
+//         }
+//     ];
 
-    const [hookProps] = useState({
-        tabs: userInfoData?.role === "ROLE_TENANT" ? defaultTabs : [...defaultTabs, {
-            label:
-                <div className="flex gap-x-2 items-center">
-                    <BsHouse size="16" />
-                    <span>Căn hộ</span>
-                </div>,
-            id: 'follows',
-            link: `/profile/${username}/assets`
-        }
-        ],
-        initialTabId: 'Matches',
-    });
-    const framer = useTabs(hookProps);
+//     const [hookProps] = useState({
+//         tabs: userInfoData?.role === "ROLE_TENANT" ? defaultTabs : [...defaultTabs, {
+//             label:
+//                 <div className="flex gap-x-2 items-center">
+//                     <BsHouse size="16" />
+//                     <span>Căn hộ</span>
+//                 </div>,
+//             id: 'follows',
+//             link: `/profile/${username}/assets`
+//         }
+//         ],
+//         initialTabId: 'Matches',
+//     });
+//     const framer = useTabs(hookProps);
 
-    return (
-        <div className="w-full flex flex-col shadow">
-            <div className="border-b w-full items-center flex border-border">
-                <Framer.Tabs {...framer.tabProps} />
-            </div>
-        </div>
-    );
-};
+//     return (
+//         <div className="w-full flex flex-col shadow">
+//             <div className="border-b w-full items-center flex border-border">
+//                 <Framer.Tabs {...framer.tabProps} />
+//             </div>
+//         </div>
+//     );
+// };
 
-export default UserTab;
+// export default UserTab;
--- hooks/index.ts
+++ hooks/index.ts
@@ -1,75 +1,75 @@
-import { ReactNode, useEffect, useState } from 'react';
-import { IUserRegister } from '@/lib/types/interfaces';
-import { IRegisterAccountForm } from '@/lib/types/interfaces/Register';
+// import { ReactNode, useEffect, useState } from 'react';
+// import { IUserRegister } from '@/lib/types/interfaces';
+// import { IRegisterAccountForm } from '@/lib/types/interfaces/Register';
 
-export type Tab = { label: ReactNode; id: string, link: string };
+// export type Tab = { label: ReactNode; id: string, link: string };
 
-export const useTabs = ({
-    tabs,
-    initialTabId,
-    onChange,
-}: {
-    tabs: Tab[];
-    initialTabId: string;
-    onChange?: (id: string) => void;
-}) => {
-    const [[selectedTabIndex, direction], setSelectedTab] = useState(() => {
-        const indexOfInitialTab = tabs.findIndex((tab) => tab.id === initialTabId);
-        return [indexOfInitialTab === -1 ? 0 : indexOfInitialTab, 0];
-    });
+// export const useTabs = ({
+//     tabs,
+//     initialTabId,
+//     onChange,
+// }: {
+//     tabs: Tab[];
+//     initialTabId: string;
+//     onChange?: (id: string) => void;
+// }) => {
+//     const [[selectedTabIndex, direction], setSelectedTab] = useState(() => {
+//         const indexOfInitialTab = tabs.findIndex((tab) => tab.id === initialTabId);
+//         return [indexOfInitialTab === -1 ? 0 : indexOfInitialTab, 0];
+//     });
 
-    return {
-        tabProps: {
-            tabs,
-            selectedTabIndex,
-            onChange,
-            setSelectedTab,
-        },
-        selectedTab: tabs[selectedTabIndex],
-        contentProps: {
-            direction,
-            selectedTabIndex,
-        },
-    };
-}
+//     return {
+//         tabProps: {
+//             tabs,
+//             selectedTabIndex,
+//             onChange,
+//             setSelectedTab,
+//         },
+//         selectedTab: tabs[selectedTabIndex],
+//         contentProps: {
+//             direction,
+//             selectedTabIndex,
+//         },
+//     };
+// }
 
 
 
-export const useUserForm = (initialUser: IRegisterAccountForm) => {
-    const [user, setUser] = useState<IUserRegister>(initialUser);
+// export const useUserForm = (initialUser: IRegisterAccountForm) => {
+//     const [user, setUser] = useState<IUserRegister>(initialUser);
 
-    const updateUser = (updates: Partial<IRegisterAccountForm>) => {
-        setUser((prevUser) => ({
-            ...prevUser,
-            ...updates,
-        }));
-    };
+//     const updateUser = (updates: Partial<IRegisterAccountForm>) => {
+//         setUser((prevUser) => ({
+//             ...prevUser,
+//             ...updates,
+//         }));
+//     };
 
-    const updateAccount = (updates: Partial<IUserRegister['account']>) => {
-        setUser((prevUser) => ({
-            ...prevUser,
-            account: {
-                ...prevUser.account,
-                ...updates,
-            },
-        }));
-    };
+//     const updateAccount = (updates: Partial<IUserRegister['account']>) => {
+//         setUser((prevUser) => ({
+//             ...prevUser,
+//             account: {
+//                 ...prevUser.account,
+//                 ...updates,
+//             },
+//         }));
+//     };
 
-    return { user, updateUser, updateAccount };
-};
+//     return { user, updateUser, updateAccount };
+// };
 
-export const useDebounce = <T,>(value: T, delay: number): T => {
-    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+// export const useDebounce = <T,>(value: T, delay: number): T => {
+//     const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delay);
+//     useEffect(() => {
+//         const handler = setTimeout(() => {
+//             setDebouncedValue(value);
+//         }, delay);
 
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [value, delay]);
+//         return () => {
+//             clearTimeout(handler);
+//         };
+//     }, [value, delay]);
 
-    return debouncedValue;
-}
+//     return debouncedValue;
+// }
