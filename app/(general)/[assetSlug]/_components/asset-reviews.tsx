9cecabe98247e71e549ebf463adef5021189fade
Genji-kun <2051052097phat@ou.edu.vn>
2024-06-08 13:02:26 +0700

    [Update] Update

--- app/(general)/[assetSlug]/_components/asset-reviews.tsx
+++ app/(general)/[assetSlug]/_components/asset-reviews.tsx
@@ -3,38 +3,41 @@
 import { useAssetDetailContext } from '@/contexts/asset-detail-context';
 import React from 'react'
 import ReviewItem from './review-item';
+import { Loader2 } from 'lucide-react';
 
 function AssetReviews() {
 
-    const { reviews } = useAssetDetailContext();
+    const { reviews, isFetchingReviews } = useAssetDetailContext();
 
     return (
         <>
             {
-                reviews && reviews.length > 0 &&
-                <div className="flex flex-col gap-4 p-6 rounded-lg border shadow-light-theme shadow-dark-theme">
-                    <h1 className="text-2xl font-semibold font-montserrat">Đánh giá của người dùng</h1>
-                    <div className=" flex flex-col gap-4">
-                        <>
-                            {
-                                reviews.map(review => {
-                                    return <React.Fragment key={review.id}>
-                                        <ReviewItem review={review} />
-                                    </React.Fragment>
-                                })
-                            }
-                        </>
-                    </div>
+                !isFetchingReviews ? <>
+
+                    {
+                        reviews && reviews.length > 0 &&
+                        <div className="flex flex-col gap-4 p-6 rounded-lg border shadow-light-theme shadow-dark-theme">
+                            <h1 className="text-2xl font-semibold font-montserrat">Đánh giá của người dùng</h1>
+                            <div className=" flex flex-col gap-4">
+                                <>
+                                    {
+                                        reviews.map(review => {
+                                            return <React.Fragment key={review.id}>
+                                                <ReviewItem review={review} />
+                                            </React.Fragment>
+                                        })
+                                    }
+                                </>
+                            </div>
+                        </div>
+
+                    }
+                </> : <div className='flex items-center justify-center'>
+                    <Loader2 className='text-primary animate-spin' />
                 </div>
-
             }
+
         </>
-    )
-
-    return (
-        <div>
-
-        </div>
     )
 }
 
